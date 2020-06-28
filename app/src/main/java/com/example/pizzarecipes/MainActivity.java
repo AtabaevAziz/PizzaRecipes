290d6bc6614f36b3f448d5058f13bc749da17563
aziz <19atabaev.aziz@gmail.com>
2020-06-28 11:50:28 +0500

    Pizza Recipes

--- app/src/main/java/com/example/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recycledView;
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<PizzaRecipeItem> pizzaRecipeItems = new ArrayList<>();
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_1,
+                Utils.PIZZA_1_TITLE, Utils.PIZZA_1_DESCRIPTION, Utils.PIZZA_1_RECIPE));
     }
 }
